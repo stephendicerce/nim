69504ef43919f32ed14e5fe2b1824a413e2e20b8
Stephen DiCerce <sdicerce@oswego.edu>
2018-07-13 22:42:26 -0400

    Changes to debug file

--- src/GameDebug.java
+++ src/GameDebug.java
@@ -4,37 +4,46 @@ import java.util.Scanner;
 public class GameDebug {
 
     public static void main(String args[]) throws Exception{
-        NimGame game = new NimGame();
+        NimGame game;
         Scanner kb = new Scanner(System.in);
         String input;
         int numberOfCoins;
         boolean coinsSet;
 
-        while(!game.getIsGameOver()) {
-            coinsSet = false;
-            numberOfCoins = 0;
-            game.showCoinsLeft();
+        System.out.println("Please enter \"1\" to play against the AI or \"2 to play against a friend.");
+        input = kb.nextLine();
 
-            do {
-                System.out.println("Enter how many coins you would like to take between 1 and 3");
-                input = kb.nextLine();
-                try {
-                    numberOfCoins = Integer.parseInt(input);
-                    if(numberOfCoins>0 && numberOfCoins<4) {
-                        coinsSet = true;
-                    } else {
-                        System.out.println("You didn't enter a correct amount of coins, try again.");
+        if(input.equals("1")) {
+            while (!game.getIsGameOver()) {
+                coinsSet = false;
+                numberOfCoins = 0;
+                game.showCoinsLeft();
+
+                do {
+                    System.out.println("Enter how many coins you would like to take between 1 and 3");
+                    input = kb.nextLine();
+                    try {
+                        numberOfCoins = Integer.parseInt(input);
+                        if (numberOfCoins > 0 && numberOfCoins < 4) {
+                            coinsSet = true;
+                        } else {
+                            System.out.println("You didn't enter a correct amount of coins, try again.");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("You didn't enter a number, please try again.");
                     }
-                } catch (NumberFormatException e) {
-                    System.out.println("You didn't enter a number, please try again.");
-                }
-            } while(!coinsSet);
+                } while (!coinsSet);
 
-            game.removeCoins(numberOfCoins);
-            game.showCoinsLeft();
-            game.aiTurn();
+                game.removeCoins(numberOfCoins);
+                game.showCoinsLeft();
+                game.aiTurn();
+            }
+
+            System.out.println("GAME OVER YOU LOSE!");
+        } else if(input.equals("2")) {
+
+        } else {
+            System.out.println("You entered an incorrect command try again");
         }
-
-        System.out.println("GAME OVER YOU LOSE!");
     }
 }
